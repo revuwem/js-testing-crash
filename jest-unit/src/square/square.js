b9f1bc1230525d66549f84178f3b5953cc577e55
Carina <revuwem@gmail.com>
2022-09-27 00:10:26 +0400

    test(jest-unit): expect checkers and func mocking

---
+++ jest-unit/src/square/square.js
@@ -0,0 +1,3 @@
+const square = (num) => (num === 1 ? 1 : Math.pow(num, 2));
+
+module.exports = square;
---
+++ jest-unit/src/square/square.test.js
@@ -0,0 +1,25 @@
+const square = require("./square");
+
+describe("square", () => {
+  test("correct", () => {
+    expect(square(2)).toBe(4);
+    expect(square(2)).toBeLessThan(5);
+    expect(square(2)).toBeGreaterThan(3);
+    expect(square(2)).not.toBeUndefined();
+  });
+
+  test("pow should be called", () => {
+    const spyMathPow = jest.spyOn(Math, "pow");
+    square(2);
+    expect(spyMathPow).toBeCalledTimes(1);
+  });
+
+  test("pow should not be called", () => {
+    const spyMathPow = jest.spyOn(Math, "pow");
+    square(1);
+    expect(spyMathPow).toBeCalledTimes(0);
+  });
+
+  //   clear Math.pow mock after each test run
+  afterEach(() => jest.clearAllMocks());
+});
