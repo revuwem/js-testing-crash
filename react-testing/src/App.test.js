7e37f7e0a010ad08be88a6c3ac8dacf0cf07ffdd
Carina <revuwem@gmail.com>
2022-09-27 23:30:41 +0400

    test(App): rendering, events, async events

--- react-testing/src/App.test.js
+++ react-testing/src/App.test.js
@@ -1,8 +1,56 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import React from "react";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from "./App";
+
+describe("App", () => {
+  test("should render all elements", () => {
+    render(<App />);
+    const helloWorldElement = screen.getByText(/hello world/i);
+    expect(helloWorldElement).toBeInTheDocument();
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+    const input = screen.getByPlaceholderText(/type something/i);
+    expect(input).toBeInTheDocument();
+    // screen.debug();
+    expect(input).toMatchSnapshot();
+  });
+
+  test("should not render 'hello2'", () => {
+    render(<App />);
+    const unwantedElement = screen.queryByText(/hello world2/i);
+    expect(unwantedElement).toBeNull();
+  });
+
+  test("should render data container", async () => {
+    render(<App />);
+    const dataContainer = await screen.findByText(/data/i);
+    expect(dataContainer).toBeInTheDocument();
+    expect(dataContainer).toHaveStyle({ color: "red" });
+  });
+
+  test("toggle button should toggle", () => {
+    render(<App />);
+    const toggleBtn = screen.getByTestId("toggle-btn");
+    expect(screen.queryByTestId("toggle-info")).toBeNull();
+    fireEvent.click(toggleBtn);
+    expect(screen.queryByTestId("toggle-info")).toBeInTheDocument();
+    fireEvent.click(toggleBtn);
+    expect(screen.queryByTestId("toggle-info")).toBeNull();
+  });
+
+  test("input value should change", () => {
+    render(<App />);
+    const inputValue = "something";
+    const input = screen.getByPlaceholderText(/type something\.\.\./i);
+    expect(screen.queryByTestId("input-value")).toContainHTML("");
+
+    // fires explicit event
+    // fireEvent.input(input, { target: { value: "something" } });
+
+    // imitates user behaviour
+    userEvent.type(input, inputValue);
+    expect(screen.queryByTestId("input-value")).toContainHTML(inputValue);
+  });
 });
