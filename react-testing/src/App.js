136c157e2187142d7a07ac7f24cb636cc34a634e
Carina <revuwem@gmail.com>
2022-09-27 23:28:45 +0400

    feat(react-testing): create App

--- react-testing/src/App.js
+++ react-testing/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState(null);
+  const [toggle, setToggle] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+
+  const onToggleClick = () => setToggle((prev) => !prev);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setData({});
+    }, 500);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
+      <button data-testid="toggle-btn" onClick={onToggleClick}>
+        Click me
+      </button>
+      <input
+        type="text"
+        name=""
+        id=""
+        onChange={(e) => setInputValue(e.target.value)}
+        placeholder="Type something..."
+      />
+      {data && <div style={{ color: "red" }}>data</div>}
+      {toggle && <span data-testid="toggle-info">toggled</span>}
+      <span data-testid="input-value">{inputValue}</span>
     </div>
   );
 }
