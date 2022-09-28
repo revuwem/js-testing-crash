0000408f190d9e4c12c2e7b0e3ac1e9e0e713e6e
Carina <revuwem@gmail.com>
2022-09-29 00:26:41 +0400

    refactor(react-testing): create Navbar component

--- react-testing/src/App.js
+++ react-testing/src/App.js
@@ -1,35 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
 import AppRouter from "./components/app-router/AppRouter";
 
 const App = () => {
   return (
     <div>
-      <ul>
-        <li>
-          <Link to="/" data-testid="link-home">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link to="/about" data-testid="link-about">
-            About
-          </Link>
-        </li>
-        <li>
-          <Link to="/dashboard" data-testid="link-dashboard">
-            Dashboard
-          </Link>
-        </li>
-        <li>
-          <Link to="/users" data-testid="link-users">
-            Users
-          </Link>
-        </li>
-      </ul>
-
+      <Navbar />
       <hr />
-
       <AppRouter />
     </div>
   );
---
+++ react-testing/src/components/navbar/Navbar.js
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <ul>
+      <li>
+        <Link to="/" data-testid="link-home">
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link to="/about" data-testid="link-about">
+          About
+        </Link>
+      </li>
+      <li>
+        <Link to="/dashboard" data-testid="link-dashboard">
+          Dashboard
+        </Link>
+      </li>
+      <li>
+        <Link to="/users" data-testid="link-users">
+          Users
+        </Link>
+      </li>
+    </ul>
+  );
+}
